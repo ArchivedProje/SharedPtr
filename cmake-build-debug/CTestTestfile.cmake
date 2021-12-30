287862a7e5f3bfd3eddb741ca9ffbe726a1d2009
Ivan Kotov <72094319+byteihq@users.noreply.github.com>
2021-12-30 11:49:46 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/byteihq/CLionProjects/resume/SharedPtr
-# Build directory: /home/byteihq/CLionProjects/resume/SharedPtr/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/home/byteihq/CLionProjects/resume/SharedPtr/cmake-build-debug/tests")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/byteihq/CLionProjects/resume/SharedPtr/CMakeLists.txt;42;add_test;/home/byteihq/CLionProjects/resume/SharedPtr/CMakeLists.txt;0;")
